c9b355e6a961db4989bcf3b29d0654100ea96581
Appani Vignesh <appanivignesh@Mac-4194.lan>
2026-01-27 18:14:48 -0500

    updated the profile icon

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
-// Components
 import Header from './Header';
-// hooks
 import useDarkMode from '../hooks/useDarkMode';
-
-// -----------------------------------------------------------
 
 export default function MainLayout({ children, meta }) {
   const { darkModeEnabled, toggleMode } = useDarkMode();
 
   return (
     <>
-      <Head>{meta}</Head>
+      <Head>
+        {meta}
+
+         <link rel="icon" type="image/png" sizes="32x32" href="/vikky.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/vikky.png" />
+  <link rel="shortcut icon" href="/vikky.png" />
+  <link rel="apple-touch-icon" href="/vikky.png" />
+      </Head>
 
       <Header darkModeEnabled={darkModeEnabled} toggleMode={toggleMode} />
-
       <main className="overflow-x-hidden">{children}</main>
     </>
   );
 }
-
-// ---------------------------------------------------------------
 
 MainLayout.propTypes = {
   children: PropTypes.node,
